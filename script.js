9d82bcf010dc820d5aeeb637f1e5a4723f6a9ebb
ZiadSakr513 <153561598+ZiadSakr513@users.noreply.github.com>
2024-02-16 03:02:14 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("menu").addEventListener("click",function(){
+    
+   if(document.getElementById("popup").style.display === ""){
+       
+       document.getElementById("popup").style.display = "block";
+       
+   }
+    else{
+        document.getElementById("popup").style.display = "";
+    }
+    
+})
